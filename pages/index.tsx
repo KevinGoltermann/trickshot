d52fa611d42f057685ca71f2814091877da3d5ba
KevinGoltermann <kevin.goltermann@datadoghq.com>
2022-07-18 23:13:30 -0600

    setup video card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,35 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import axios from 'axios';
+import { Video } from '../types';
+import VideoCard from '../components/VideoCard';
+import NoResults from '../components/NoResults';
 
-const Home: NextPage = () => {
+interface IProps {
+  videos: Video[]
+}
+
+const Home = ({ videos }: IProps) => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className='felx flex-col gap-10 video h-full'>
+      {videos.length ? (
+        videos.map((video: Video) => (
+          <VideoCard post={video} key={video._id}/>
+        ))
+      ) : (
+          <NoResults text={'No Videos'}/>
+      )}
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data
+    }
+  }
 }
 
 export default Home
