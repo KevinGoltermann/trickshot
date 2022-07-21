f8bfa17b52515f50407dbb805d246e0cb2d0f8cc
KevinGoltermann <kevin.goltermann@datadoghq.com>
2022-07-20 22:55:09 -0600

    implement google oauth with no state yet

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
-import { AIOutlineLogout } from 'react-icons/ai';
+import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
-import Logo from '../utils/tiktik-logo.png'
+import Logo from '../utils/tiktik-logo.png';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
 
 const Navbar = () => {
+
+const user = false;
+
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
         <Link href="/">
@@ -21,6 +24,16 @@ const Navbar = () => {
                 />
             </div>
         </Link>
+
+        <div>SEARCH</div>
+
+        <div>
+          {user ? (
+            <div>Logged In</div>
+          ) : (
+            <GoogleLogin onSuccess={(response) => console.log(response)} onError={() => console.log('Error')} />
+          )}
+        </div>
     </div>
   )
 }
