d087eb3b5c0ab50b6cff80caee9cef8df0ad9346
KevinGoltermann <kevin.goltermann@datadoghq.com>
2022-08-12 10:23:57 -0600

    add Datadog RUM application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import { Sidebar } from '../components/Sidebar';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { datadogRum } from '@datadog/browser-rum';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [isSSR, setIsSSR] = useState(true);
@@ -13,6 +14,22 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   }, []);
   
   if(isSSR) return null;
+
+  datadogRum.init({
+    applicationId: 'eecf2d04-10bc-4798-87ae-1fb06c0ff642',
+    clientToken: 'pub4794e32a101517ed468b2c7a354807e4',
+    site: 'datadoghq.com',
+    service:'trickshot',
+    env:'prod',
+    // Specify a version number to identify the deployed version of your application in Datadog 
+    // version: '1.0.0',
+    sampleRate: 100,
+    premiumSampleRate: 100,
+    trackInteractions: true,
+    defaultPrivacyLevel:'mask-user-input'
+});
+    
+datadogRum.startSessionReplayRecording();
 
   return (
     <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
